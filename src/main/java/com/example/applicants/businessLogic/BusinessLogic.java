4b24937889ac3e2864cf186eb8512d75fccbc01b
Adam Stewart <adamstewart94@gmail.com>
2021-10-26 15:23:46 +0000

    Business logic junit test

--- src/main/java/com/example/applicants/businessLogic/BusinessLogic.java
+++ src/main/java/com/example/applicants/businessLogic/BusinessLogic.java
@@ -2,9 +2,11 @@ package com.example.applicants.businessLogic;
 
 public class BusinessLogic {
 
-    public double calculateQuote(Double typeFactor, Double engineSizeFactor, Double additionalDriversFactor, Double commericalUseFactor, Double outsideStateFactor, Double vehicleValueFactor){
 
-        return (100 * typeFactor * engineSizeFactor * additionalDriversFactor * commericalUseFactor * outsideStateFactor * vehicleValueFactor);
+
+    public double calculateQuote(Double typeFactor, Double engineSizeFactor, Double additionalDriversFactor, Double commercialUseFactor, Double outsideStateFactor, Double vehicleValueFactor){
+
+        return (100 * typeFactor * engineSizeFactor * additionalDriversFactor * commercialUseFactor * outsideStateFactor * vehicleValueFactor);
 
     }
 }
